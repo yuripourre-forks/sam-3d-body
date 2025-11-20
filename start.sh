12e60b5d0859ec82e984e29d8d195083d5136017
Yuri Pourre <yuripourre@gmail.com>
2025-11-20 15:03:09 -0800

    Add start script

---
+++ start.sh
@@ -0,0 +1,4 @@
+conda activate sam_3d_body
+
+python main.py     --image_folder /home/supergamebuilder/images     --output_folder /home/supergamebuilder/images/output     --checkpoint_path ./checkpoints/sam-3d-body-dinov3/model.ckpt     --mhr_path ./checkpoints/sam-3d-body-dinov3/assets/mhr_model.pt
+
